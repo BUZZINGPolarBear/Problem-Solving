468ae4c2d8dd2868c8da24c07a225119e3c34be2
Jeonjunhowi <jjh63360@kau.kr>
2022-11-06 01:18:58 +0900

    add: 20221106 BOJ 2

---
+++ BOJ PS/리그 오브 레전설 (Small).cpp
@@ -0,0 +1,8 @@
+//
+//  리그 오브 레전설 (Small).cpp
+//  Problem_Solving
+//
+//  Created by joonhwi on 2022/11/05.
+//
+
+#include <stdio.h>
---
+++ BOJ PS/이건 꼭 풀어야해.cpp
@@ -0,0 +1,8 @@
+//
+//  이건 꼭 풀어야해.cpp
+//  Problem_Solving
+//
+//  Created by joonhwi on 2022/11/05.
+//
+
+#include <stdio.h>
